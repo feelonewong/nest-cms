a57e3863eb53f3e687a92b2e359f48f74a10ff8e
feelonewong <feelonewong@gmail.com>
2023-06-26 17:39:08 +0800

    update:2-09自定义表单验证

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('register')
+  create(@Body() dto: RegisterDto) {
+    return dto;
+  }
+}
